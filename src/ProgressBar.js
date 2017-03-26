4b442d469eb813e68e4128ebbe29ecf56fde9e10
Brandon <booth.brandon4@gmail.com>
2017-03-26 19:27:52 -0400

    added a base hardcoded barchart

--- src/ProgressBar.js
+++ src/ProgressBar.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
 // import { Button } from 'react-materialize';
-import { Timeline } from 'react-chartkick';
+import { BarChart } from 'react-chartkick';
 import '../styles/App.css'
 
 class ProgressBar extends Component {
   render() {
     return (
       <div className='timeline-container'>
-        <Timeline data={[["Washington", "1789-04-29", "1797-03-03"],["Adams", "1797-03-03", "1801-03-03"]]} />
+        <BarChart data={[{
+            name: "Completed Tasks", 
+            data: [["Photography", 50], ["Catering", 20]]
+          },
+          {
+            name: "Uncompleted Tasks", 
+            data: [["Photography", 50], ["Catering", 80]]
+          }]}
+          max={100}
+          stacked={true}/>
       </div>
     );
   }
