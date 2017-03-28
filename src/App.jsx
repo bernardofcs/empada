6545398daad53d6b8095c835e4f1d122e44d55c2
= <ammarmasud@gmail.com>
2017-03-28 18:54:27 -0400

    done almost everything before the next step: pulling data from server rather than using local sample array

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
 import React, { Component } from 'react';
 import ReactInterval from 'react-interval';
-// import { Row, Col } from 'react-materialize';
+// import { Col } from 'react-materialize'
 import './App.css';
-import InsertForm from './InsertForm.js'
-
 
 /*
 Users:
@@ -24,84 +22,75 @@ class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-      count: 1,
       grace_period: 300000,
-      newsfeed: [
-        {
-          type:   'assigned_user_action',
-          user:   'assigned_user',
-          task:   'assigned_task',
-          action: 'started_task', //'completed_task', 'progress_bar'
-          action_time: new Date(2016, 11, 1, 9),
-          assigned_time: new Date(2016, 11, 1, 9),
-          notification_time: new Date(2016, 11, 1, 9)
-        },
-        {
-          type:   'assigned_user_action',
-          user:   'assigned_user',
-          task:   'assigned_task',
-          action: 'completed_task',
-          action_time: new Date(2016, 11, 1, 9, 10),
-          assigned_time: new Date(2016, 11, 1, 9, 0),
-          notification_time: new Date(2016, 11, 1, 9, 10)
-        },
-        {
-          type:   'assigned_user_action',
-          user:   'assigned_user',
-          task:   'assigned_task',
-          action: 'started_task', //'completed_task', 'progress_bar'
-          action_time: new Date(2016, 11, 1, 9, 1),
-          assigned_time: new Date(2016, 11, 1, 9, 10),
-          notification_time: new Date(2016, 11, 1, 9, 10)
-        },
-        {
-          type:   'task_timing',
-          user:   'assigned_user',
-          task:   'assigned_task',
-          action: 'task_not_started', //'task_not_completed'
-          assigned_time: new Date(2016, 11, 1, 9, 10),
-          notification_time: new Date(2016, 11, 1, 9)
-        },
-        {
-          type:   'task_timing',
-          user:   'assigned_user',
-          task:   'assigned_task',
-          action: 'task_not_completed',
-          assigned_time: new Date(2016, 11, 1, 9, 10),
-          notification_time: new Date(2016, 11, 1, 9)
-        },
-        {
-          type:   'project_progress',
-          action: 'percent_completed', //'percent_expected' eg.Should be 50% at this point
-          notification_time: new Date(2016, 11, 1, 9)
-        }
-      ]
+      newsfeed: []
     };
   }
 
-  incrementCounter = () => {
-    return this.setState({count: this.state.count + 1})
-  }
+  updateNewsFeed = () => {
+    let fromDb = [
+      {
+        type:   'assigned_user_action',
+        user:   'assigned_user',
+        task:   'assigned_task',
+        action: 'started_task', //'completed_task', 'progress_bar'
+        action_time: new Date(2016, 11, 1, 9),
+        assigned_time: new Date(2016, 11, 1, 9),
+        notification_time: new Date(2016, 11, 1, 9)
+      },
+      {
+        type:   'assigned_user_action',
+        user:   'assigned_user',
+        task:   'assigned_task',
+        action: 'completed_task',
+        action_time: new Date(2016, 11, 1, 9, 10),
+        assigned_time: new Date(2016, 11, 1, 9, 0),
+        notification_time: new Date(2016, 11, 1, 9, 10)
+      },
+      {
+        type:   'assigned_user_action',
+        user:   'assigned_user',
+        task:   'assigned_task',
+        action: 'started_task', //'completed_task', 'progress_bar'
+        action_time: new Date(2016, 11, 1, 9, 1),
+        assigned_time: new Date(2016, 11, 1, 9, 10),
+        notification_time: new Date(2016, 11, 1, 9, 10)
+      },
+      {
+        type:   'task_timing',
+        user:   'assigned_user',
+        task:   'assigned_task',
+        action: 'task_not_started', //'task_not_completed'
+        assigned_time: new Date(2016, 11, 1, 9, 10),
+        notification_time: new Date(2016, 11, 1, 9, 15)
+      },
+      {
+        type:   'task_timing',
+        user:   'assigned_user',
+        task:   'assigned_task',
+        action: 'task_not_completed',
+        assigned_time: new Date(2016, 11, 1, 9, 10),
+        notification_time: new Date(2016, 11, 1, 9)
+      },
+      {
+        type:   'project_progress',
+        action: 'percent_completed', //'percent_expected' eg.Should be 50% at this point
+        percantage: '50',
+        notification_time: new Date(2016, 11, 1, 10)
+      },
+      {
+        type:   'project_progress',
+        action: 'percent_expected',
+        percantage: '50',
+        notification_time: new Date(2016, 11, 1, 10)
+      }
+    ]
+    // this.state.newsfeed
 
-
-
-  handleSubmit = (e) => {
-    e.preventDefault();
-    // console.log('submitted')
-   console.log(this.state.insert)
-  }
-
-  handleChange = (e) => {
-    e.preventDefault();
-    console.log(e.target.value)
-    this.setState({insert: e.target.value})
-  }
-  render() {
-    const {count} = this.state;
-    const NewsFeed = this.state.newsfeed.map((item, index) => {
+    return this.setState({newsfeed: fromDb.map((item, index) => {
+      let notif_type;
       switch(item.type) {
         case 'assigned_user_action':
-          let notif_type;
           if (item.action === 'started_task') {
             notif_type = ['Started', 'start'];
           } else if (item.action === 'completed_task') {
@@ -123,7 +112,7 @@ class App extends Component {
               <div className="collapsible-header">
                 <span className={`new badge ${notif_type[2]}`} data-badge-caption={`${notif_type[3]}`}></span>
                 <i className="material-icons">{`${notif_type[4]}`}</i>
-                {`${item.user} has ${item.action}`}
+                <p>{`${item.user} has ${item.action}`}</p>
               </div>
               <div className="collapsible-body left-align">
                 <dl>
@@ -136,57 +125,68 @@ class App extends Component {
           );
 
         case 'task_timing':
-          if (new Date() - item.assigned_time > this.state.grace_period) {
+          if (item.action === 'task_not_started') {
+            notif_type = ['Started', 'start'];
+          } else if (item.action === 'task_not_completed') {
+            notif_type = ['Completed', 'completion'];
+          }
+          notif_type.push('late', 'thumb_down');
 
+          return (
+            <li>
+              <div className="collapsible-header">
+                <span className="new badge red" data-badge-caption={`${notif_type[2]}`}></span>
+                <i className="material-icons">{`${notif_type[3]}`}</i>
+                <p>{`${item.user} has not ${notif_type[0].toLowerCase()} ${item.task}`}</p>
+              </div>
+              <div className="collapsible-body left-align">
+                <dl>
+                  <dt><b>Task:</b> {item.task}</dt>
+                  <dt><b>Expected {`${notif_type[1]}`} time:</b> {`${item.assigned_time}`}</dt>
+                </dl>
+              </div>
+            </li>
+          );
+
+        case 'project_progress':
+          notif_type = [];
+          if (item.action === 'percent_completed') {
+            notif_type.push('', 'blue', 'on time', 'alarm_on');
+          } else if (item.action === 'percent_expected') {
+            notif_type.push('expected to be ', 'red', 'late', 'thumb_down');
           }
 
           return (
             <li>
-              <div className="collapsible-header"><span className="badge">1</span><i className="material-icons">place</i>Second</div>
-              <div className="collapsible-body"><p>Lorem ipsum dolor sit amet.</p></div>
+              <div className="collapsible-header">
+                <span className={`new badge ${notif_type[1]}`} data-badge-caption={`${notif_type[2]}`}></span>
+                <i className="material-icons">{`${notif_type[3]}`}</i>
+                <p>{`${item.percent} of project ${notif_type[0]}complete.`}</p>
+              </div>
             </li>
           );
-        case 'project_progress':
-          return (
-            <li>
-              <div className="collapsible-header"><span className="badge">1</span><i className="material-icons">place</i>Second</div>
-              <div className="collapsible-body"><p>Lorem ipsum dolor sit amet.</p></div>
-            </li>
-          );
+
         default:
           throw new Error(`Unknown event type in newsfeed: ${item.type}`);
       }
-    });
+    })})
+  }
 
+  componentDidMount() {
+    this.updateNewsFeed();
+  }
+
+  render() {
+    const { newsfeed } = this.state;
 
     return (
-      <div className="App">
-        <div className="App-header">
-          <h2>Welcome to EMPADA</h2>
+      <div className="App row">
+        <div className="col s4 offset-s8">
+          <ul className="collapsible popout" data-collapsible="accordion">
+            {newsfeed}
+            <ReactInterval timeout={30000} enabled={true} callback={this.updateNewsFeed} />
+          </ul>
         </div>
-        <br />
-        <InsertForm handleChange={this.handleChange} handleSubmit={this.handleSubmit} />
-        {count}
-        <ReactInterval timeout={30000} enabled={true} callback={this.incrementCounter} />
-
-        <ul className="collapsible popout" data-collapsible="accordion">
-          {NewsFeed}
-          <li>
-            <div className="collapsible-header">
-              <span className="new badge">4</span>
-              <i className="material-icons">filter_drama</i>
-              First
-            </div>
-            <div className="collapsible-body">
-              <p>Lorem ipsum dolor sit amet.</p>
-            </div>
-          </li>
-          <li>
-            <div className="collapsible-header"><span className="badge">1</span><i className="material-icons">place</i>Second</div>
-            <div className="collapsible-body"><p>Lorem ipsum dolor sit amet.</p></div>
-          </li>
-        </ul>
-
       </div>
     );
   }
