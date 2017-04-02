287588f358d147d8e9e70855b928f6f328e12c35
bernardofcs <bernardofcs@hotmail.com>
2017-04-02 19:36:48 -0400

    added project selection to navbar

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -4,7 +4,8 @@ class Nav extends Component {
   render(){
     return(
     <div>
-      <button onClick={this.props.displayEventCreationFormPage}>Create Event Form</button>      
+      <button onClick={this.props.displayProjectSelectionPage}>Select Project</button>
+      <button onClick={this.props.displayEventCreationFormPage}>Create Project Form</button>      
       <button onClick={this.props.displayDashboardPage}>Dashboard</button>      
     </div>
     ) 
