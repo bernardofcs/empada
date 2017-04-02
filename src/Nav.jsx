f22e06bd1fdec2370b3b5cb03dde22ea439b8ff9
bernardofcs <bernardofcs@hotmail.com>
2017-04-01 20:52:54 -0400

    merged news feed and dashboard

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -6,7 +6,6 @@ class Nav extends Component {
     <div>
       <button onClick={this.props.displayEventCreationFormPage}>Create Event Form</button>      
       <button onClick={this.props.displayDashboardPage}>Dashboard</button>      
-      <button onClick={this.props.displayNewsFeedPage}>News Feed</button>      
     </div>
     ) 
   }
