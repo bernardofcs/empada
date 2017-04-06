de3e2b7f446fc8c2eb83d7c229dd39d5d0a04ee7
Brandon <booth.brandon4@gmail.com>
2017-04-06 12:05:23 -0400

    styled some more

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -5,8 +5,8 @@ import '../styles/App.css';
 class HomePage extends Component {
   render() {
     return (
-      <div>
-        <MediaBox src="concert.jpg" caption="A demo media box1" width="100%"/>
+      <div className='align'>
+        {/*<MediaBox src="concert.jpg" caption="A demo media box1" width="100%"/>*/}
         <Slider>
           <Slide
             src="http://integratedfacilitationservices.com/wp-content/uploads/2016/02/event-planningand-management-1024x683.jpg">
@@ -31,42 +31,42 @@ class HomePage extends Component {
           <Col s={10} className='offset-s1'>
           <h4>Worry No Longer, The Solution Is Here</h4>
             <br/>
-            Stay up to date on the your contractors progress in <strong>real time</strong> with the live notifications and an innovative timeline. This helps improve communication and accountability all at once. 
+            Stay up to date on the your contractors progress in <b>real time</b> with the live notifications and an innovative timeline. This helps improve communication and accountability all from the same seamless framework. 
           </Col>
         </Row>
         <br/>
         <br/>
         <br/>
-        <div className="divider"></div>
+        <div className="divider"></div> 
         <br/>
         
         <Row>
-          <Col s={12}>
+          <Col s={12} className='icons'>
           <h4>What We Bring To The Table</h4>
           </Col>
         </Row>
         <Row>
-          <Col s={12} m={6}>
+          <Col s={12} m={6} className='icons'>
             <Icon className="big-icons">person_pin</Icon>
             <br/>
             A team of four young developers...
           </Col>
-          <Col s={12} m={6}>
+          <Col s={12} m={6} className='icons'>
             <Icon className="big-icons">business</Icon>
             <br/>
             interested in learning about your business... 
           </Col>
         </Row>
         <Row>
-          <Col s={12} m={6}>
+          <Col s={12} m={6} className='icons'>
             <Icon className="big-icons">trending_up</Icon>
             <br/>
             who are dedicated to your continued success...
           </Col>
-          <Col s={12} m={6} >
+          <Col s={12} m={6}>
             <Icon className="big-icons">web</Icon>
             <br/>
-            and have a whole 2 months of web experience to back us up.
+            <p className='icons'>and have a whole 2 months of web experience to back us up.</p>
           </Col>
         </Row>
         <br/>
@@ -78,32 +78,28 @@ class HomePage extends Component {
         <Row>
           <Col s={12} m={6}>
             <Card className='pictures' header={<CardTitle reveal image={"Event-Management-Banner.jpg"} waves='light'/>}
-              title="Card Title"
-              reveal={<p>Here is some more information about this product that is only revealed once clicked on.</p>}>
-              <p><a href="#">This is a link</a></p>
+              title="Stand Out"
+              reveal={<p>A perfect way to minimize your time checking in on your contractors.</p>}>
             </Card>
           </Col>
           <Col s={12} m={6}>
             <Card className='pictures' header={<CardTitle reveal image={"concert.jpg"} waves='light'/>}
-              title="Card Title"
-              reveal={<p>Here is some more information about this product that is only revealed once clicked on.</p>}>
-              <p><a href="#">This is a link</a></p>
+              title="Enjoy"
+              reveal={<p>A perfect way to help keep you keep your clients happy.</p>}>
             </Card>
           </Col>
         </Row>
         <Row>
           <Col s={12} m={6}>
             <Card className='pictures' header={<CardTitle reveal image={"event-planning-and-management.jpg"} waves='light'/>}
-              title="Card Title"
-              reveal={<p>Here is some more information about this product that is only revealed once clicked on.</p>}>
-              <p><a href="#">This is a link</a></p>
+              title="Perfect For All Types Of Occasions"
+              reveal={<p>Perfect for all kinds of events.</p>}>
             </Card>
           </Col>
           <Col s={12} m={6}>
             <Card className='pictures' header={<CardTitle reveal image={"mic.jpg"} waves='light'/>}
-              title="Card Title"
-              reveal={<p>Here is some more information about this product that is only revealed once clicked on.</p>}>
-              <p><a href="#">This is a link</a></p>
+              title="Sit Back and Relax"
+              reveal={<p>Let your voice be heard, without having to speak.</p>}>
             </Card>
           </Col>
         </Row>
